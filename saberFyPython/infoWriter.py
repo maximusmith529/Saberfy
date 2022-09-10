d612a42705287ed8cc19349554021816f432a35f
Gabriel Suoth <gabrielsuoth@gmail.com>
2022-09-10 09:02:10 -0400

    Infowriter

--- saberFyPython/infoWriter.py
+++ saberFyPython/infoWriter.py
@@ -1,40 +1,53 @@
 import json
 from SpotifyAPI import SpotifyAPI
+
+
 class infoWriter():
     songid = None
     songName = None
     artistName = None
     BPM = None
+    offset = None
+    songLength = None
 
     def __init__(self, songid):
         self.songid = songid
 
-    def getArtist(self):
+    def getInfo(self):
         spotify = SpotifyAPI("b6709a08f53d40b6ab740ff646c017a6", "110e1f30d2f3463a8b56a06a528fbcb7")
         songDetails = spotify.get_track(self.songid)
+        songAnalysis = spotify.get_analysis(self.songid)
         self.artistName = songDetails["artists"][0]["name"]
+        self.songName = songDetails["name"]
+        self.songLength = round(songDetails["duration_ms"] / 1000)
+        self.BPM = round(songAnalysis["track"]["tempo"])
 
-    def writeInfoDat(self):
-        test = json.dumps({
-            '_version':'2.6.0',
+    def writeInfo(self):
+        info = json.dumps({
+            '_version': '2.6.0',
             '_songName': self.songName,
             '_songSubName': "",
             '_songAuthorName': self.artistName,
-            '_levelAuthorName': "saberFy",
+            '_levelAuthorName': "saberFy Automated Map",
             '_beatsPerMinute': self.BPM,
             '_shuffle': 0,
-            '_shufflePeriod': 0.5,
-            '_previewStartTime': 31.5,
+            '_shufflePeriod': 0,
+            '_previewStartTime': self.songLength / 2,
             '_previewDuration': 7,
             '_songFilename': "song.ogg",
             '_coverImageFilename': "cover.jpg",
             '_environmentName': "BigMirrorEnvironment",
             '_allDirectionsEnvironmentName': "GlassDesertEnvironment",
-            '_songTimeOffset': 0,
-            '_difficultyBeatmapSets': []
+            '_songTimeOffset': self.offset,
+            '_difficultyBeatmapSets': [{
+                '_difficulty': "ExpertPlus",
+                '_difficultyRank': 3,
+                '_beatmapFilename': "Normal.dat",
+                '_noteJumpMovementSpeed': 13,
+                '_noteJumpStartBeatOffset': 0
+            }]
         })
-        print(test)
-
+        print(info)
 
 
 """
--- saberFyPython/main.py
+++ saberFyPython/main.py
@@ -2,31 +2,22 @@
 from SpotifyAPI import SpotifyAPI
 import json
 
+from infoWriter import infoWriter
+
 if __name__ == '__main__':
-    spotify = SpotifyAPI("b6709a08f53d40b6ab740ff646c017a6", "110e1f30d2f3463a8b56a06a528fbcb7")
-    songDetails = spotify.get_analysis("0AzD1FEuvkXP1verWfaZdv?si=6972a6aa2c4a4036")
+    infoW = infoWriter("0ZNqGbEclCmKtyw3l4RrM0?si=fdce999a2bad4b6a")
+    infoW.getInfo()
+    infoW.writeInfo()
 
-    songTracks = spotify.get_track("4lv9Fj6md6hbu6eavN8EKa?si=a2d8bf3e51544820")
+    """
+    json_object = json.dumps(songDetails, indent=4)
 
-    test = json.dumps({
-        '_version': '2.6.0',
-        '_songName': "test",
-        '_songSubName': "",
-        '_songAuthorName': "test",
-        '_levelAuthorName': "saberFy",
-        '_beatsPerMinute': 120,
-        '_shuffle': 0,
-        '_shufflePeriod': 0,
-        '_previewStartTime': 31.5,
-        '_previewDuration': 7,
-        '_songFilename': "song.ogg",
-        '_coverImageFilename': "cover.jpg",
-        '_environmentName': "BigMirrorEnvironment",
-        '_allDirectionsEnvironmentName': "GlassDesertEnvironment",
-        '_songTimeOffset': 0,
-        '_difficultyBeatmapSets': []
-    })
-    print(test)
+    with open("sample.json", "w") as outfile:
+        outfile.write(json_object)
+    """
+
+
+
 
 
 
