c3f49c8dd5d6c6a8ba4074154583adc7431a8219
Gabriel Suoth <gabrielsuoth@gmail.com>
2022-09-10 09:55:48 -0400

    added temp folder

--- saberFyPython/main.py
+++ saberFyPython/main.py
@@ -6,17 +6,15 @@ from infoWriter import infoWriter
 
 if __name__ == '__main__':
     spotify = SpotifyAPI("b6709a08f53d40b6ab740ff646c017a6", "110e1f30d2f3463a8b56a06a528fbcb7")
-    songDetails = spotify.get_analysis("0AzD1FEuvkXP1verWfaZdv?si=6972a6aa2c4a4036")
+    songDetails = spotify.get_analysis("0a9sd6MEXZXIPHk0fAxpZ4?si=17c820600f7b4b1e")
 
-    songTracks = spotify.get_track("4lv9Fj6md6hbu6eavN8EKa?si=a2d8bf3e51544820")
-    for i in songTracks["artists"]:
-        print(i)
-    """
-    song_json = json.dumps(songTracks, indent=4)
-    with open("songdetail.json", "w") as outfile:
+
+
+    song_json = json.dumps(songDetails, indent=4)
+    with open("songDetails.json", "w") as outfile:
         outfile.write(song_json)
     
-    """
+
     infoW = infoWriter("0ZNqGbEclCmKtyw3l4RrM0?si=fdce999a2bad4b6a", 0)
     infoW.write()
 
