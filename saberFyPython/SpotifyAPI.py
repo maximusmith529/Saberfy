e45595d9c75070cc7dd431e8309642990a24b803
Gabriel Suoth <gabrielsuoth@gmail.com>
2022-09-10 03:21:30 -0400

    python base file

---
+++ saberFyPython/SpotifyAPI.py
@@ -0,0 +1,93 @@
+import base64
+import datetime
+import requests
+
+
+# https://github.com/codingforentrepreneurs/30-Days-of-Python/tree/master/tutorial-reference/Day%2019
+
+class SpotifyAPI(object):
+    access_token = None
+    access_token_expires = datetime.datetime.now()
+    access_token_did_expire = True
+    client_id = None
+    client_secret = None
+    token_url = "https://accounts.spotify.com/api/token"
+
+    def __init__(self, client_id, client_secret, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def get_client_credentials(self):
+        # returns base64 for client
+        client_id = self.client_id
+        client_secret = self.client_secret
+        if client_secret is None or client_id is None:
+            raise Exception("You must set client_id and client_secret")
+        client_creds = f"{client_id}:{client_secret}"
+        client_creds_b64 = base64.b64encode(client_creds.encode())
+        return client_creds_b64.decode()
+
+    def get_token_headers(self):
+        client_creds_b64 = self.get_client_credentials()
+        return {
+            "Authorization": f"Basic {client_creds_b64}"
+        }
+
+    def get_token_data(self):
+        return {
+            "grant_type": "client_credentials"
+        }
+
+    def perform_auth(self):
+        token_url = self.token_url
+        token_data = self.get_token_data()
+        token_headers = self.get_token_headers()
+        r = requests.post(token_url, data=token_data, headers=token_headers)
+        if r.status_code not in range(200, 299):
+            raise Exception("Could not authenticate client.")
+            # return False
+        data = r.json()
+        now = datetime.datetime.now()
+        access_token = data['access_token']
+        expires_in = data['expires_in']  # seconds
+        expires = now + datetime.timedelta(seconds=expires_in)
+        self.access_token = access_token
+        self.access_token_expires = expires
+        self.access_token_did_expire = expires < now
+        return True
+
+    def get_access_token(self):
+        token = self.access_token
+        expires = self.access_token_expires
+        now = datetime.datetime.now()
+        if expires < now:
+            self.perform_auth()
+            return self.get_access_token()
+        elif token == None:
+            self.perform_auth()
+            return self.get_access_token()
+        return token
+
+    def get_resource_header(self):
+        access_token = self.get_access_token()
+        headers = {
+            "Authorization": f"Bearer {access_token}"
+        }
+        return headers
+
+    def get_resource(self, lookup_id, resource_type='albums', version='v1'):
+        endpoint = f"https://api.spotify.com/{version}/{resource_type}/{lookup_id}"
+        headers = self.get_resource_header()
+        r = requests.get(endpoint, headers=headers)
+        if r.status_code not in range(200, 299):
+            return {}
+        return r.json()
+
+    def get_analysis(self, _id):
+        return self.get_resource(_id, resource_type='audio-analysis')
+
+    def get_track(self, _id):
+        return self.get_resource(_id, resource_type='tracks')
+
+    # add one for images
---
+++ saberFyPython/main.py
@@ -0,0 +1,24 @@
+
+from SpotifyAPI import SpotifyAPI
+import json
+
+if __name__ == '__main__':
+    spotify = SpotifyAPI("b6709a08f53d40b6ab740ff646c017a6", "110e1f30d2f3463a8b56a06a528fbcb7")
+    songDetails = spotify.get_analysis("0AzD1FEuvkXP1verWfaZdv?si=6972a6aa2c4a4036")
+
+    songTracks = spotify.get_track("4lv9Fj6md6hbu6eavN8EKa?si=a2d8bf3e51544820")
+    for i in songTracks["artists"]:
+        print(i)
+
+
+"""
+song_json = json.dumps(songTracks, indent=4)
+with open("songdetail.json", "w") as outfile:
+    outfile.write(song_json)
+
+"""
+
+
+
+
+
