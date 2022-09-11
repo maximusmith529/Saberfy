cd3ff0438d32e94057a5e7fad6a4ca259c28f9e3
Bryson Paul <brysonpaul1870@gmail.com>
2022-09-11 09:17:13 -0400

    web server

--- saberFyPython/main.py
+++ saberFyPython/main.py
@@ -1,3 +1,5 @@
+from http.server import HTTPServer
+
 from SpotifyAPI import SpotifyAPI
 import json
 import spotdl
@@ -6,18 +8,27 @@ from infoWriter import infoWriter
 from node import writeDiff
 import pathlib
 import zipfile
-
+from server import MyServer
+from http.server import BaseHTTPRequestHandler
 
 if __name__ == '__main__':
     spotify = SpotifyAPI("b6709a08f53d40b6ab740ff646c017a6", "110e1f30d2f3463a8b56a06a528fbcb7")
+    webServer = HTTPServer(("localhost", 8080), MyServer)
+    print("Server started http://localhost:8080")
+
+    while  not len(MyServer.path) == (23):
+        webServer.handle_request()
+
+    webServer.server_close()
+    print("Server stopped.")
+
     if os.listdir("temp/") != []:
         for i in os.listdir("temp/"):
             os.remove("temp/"+i)
-    input = input("please enter a spotify url\n")
+   # input = input("please enter a spotify url\n")
 
-    uri = input.split("/")[len(input.split("/"))-1];
-
-
+    uri = MyServer.path.split("/")[1] #input.split("/")[len(input.split("/"))-1];
+    print("the uri is"+uri)
 
     songDetails = spotify.get_analysis(uri)
     writeDiff(uri)
@@ -32,6 +43,8 @@ if __name__ == '__main__':
     with zipfile.ZipFile("song.zip", mode="w") as archive:
         for file_path in directory.iterdir():
             archive.write(file_path, arcname=file_path.name)
+
+
 
 
 
---
+++ saberFyPython/server.py
@@ -0,0 +1,22 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+    path = "/"
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+        MyServer.path = self.path
+
+
